7c945ba6c855d9a3213f3d300d43996903a278dc
Annabel Rothschild <arothsch@wellesley.edu>
2017-07-18 15:28:42 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# idkanymore
+# ColdCaller
+GWC Cold Caller Program
---
+++ coldCallerFunctions.js
@@ -0,0 +1,34 @@
+//names yet to be called, names already called
+var names = Array("bob", "joe", "fred");
+var usedNames = Array();
+//shuffle if no names left, else pull name --> remove from names and add to usedNames 
+function pullName(){
+    console.log("checking usedNames at pn:" + usedNames);
+    var index = Math.floor(Math.random() * names.length);
+    var pulledName = names[index];
+    var removeValue = names.indexOf(pulledName);
+    names.splice(removeValue, 1);
+    document.getElementById("nameText").innerHTML = pulledName;
+    usedNames.push(pulledName);
+    if (names.length < 1){
+        //console.log("time to shuffle")
+        //var temp = names.pop();
+        //console.log("name popped from names: " + temp);
+        //usedNames.push(temp);
+        console.log("un right before shuffling: " + usedNames);
+        shuffle();
+    }
+}
+//remove all in usedNames and put back in names
+function shuffle(){
+    console.log("entered shuffle, len(usedNames): " +usedNames.length);
+    for (i=0; i < usedNames.length+2; i++) {
+        console.log("un remaining in for loop: " + usedNames);
+        var temp = usedNames.pop();
+        console.log("name being added to names: " + temp);
+        names.push(temp);
+        console.log("name in names: " + names);
+    }
+    console.log("names after shuffling: " + names);
+}
+
