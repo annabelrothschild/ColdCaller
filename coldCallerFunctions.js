8a215f69dd97f138a5eebf3a3a0eec2a194fbb05
Annabel Rothschild <arothsch@wellesley.edu>
2017-11-08 14:56:08 -0500

    Update coldCallerFunctions.js
    updated top-of-file comments

--- coldCallerFunctions.js
+++ coldCallerFunctions.js
@@ -1,5 +1,10 @@
+/*ColdCaller.js, built for Girls Who Code ATL Classroom #1
+Written By: Annabel Rothschild
+Last Modified: 2017
+*/
+
 //names yet to be called, names already called
-var names = Array("bob", "joe", "fred");
+var names = Array("student1", "student2", "student3");
 var usedNames = Array();
 //shuffle if no names left, else pull name --> remove from names and add to usedNames 
 function pullName(){
